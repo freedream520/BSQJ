176a7cb5913d2333c23c6e9e137484ee7cf1b6d5
Mark <wancy86@sina.com>
2016-05-05 21:57:17 +0800

    pager 2.1

--- README.md
+++ README.md
@@ -6,9 +6,41 @@ BootStrap table pageing
 #初始化表格和分页
 $("#iplisttable").pagingTable({
     json_url: "{% url 'attendence:ip_json'%}",
-    pageSize: 2
+    pageSize: 2,
+    separator: "#" /*V2.1+*/
 });
+<hr>
+#version 2.1 changes:
+1.支持模板语法，指定分隔符，默认为 #
+2.调整html结构，更符合一般的table结构
 
+#html结构范例:
+<table id="iplisttable" class="table table-bordered table-hover table-striped">
+    <thead>
+        <tr>
+            <th width="20%">Name</th>
+            <th width="20%">IP</th>
+            <th width="20%">Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr pk="#ip#" template=1 style="display: none">
+            <td>#name#</td>
+            <td>#ip#</td>
+            <td>
+                <a href="javascript:void(0)" onclick="delIP(this)" class="delip" style="cursor:pointer">
+                    <span class="glyphicon glyphicon-floppy-remove"></span> <span>(#ip#)</span>
+                </a>
+                <a style="cursor:pointer;display:inline-block;margin-left:15px;">
+                    <span class="glyphicon glyphicon-edit"></span>
+                </a>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+<hr>
+#version 2.0
 #html结构范例:
 <table id="iplisttable" class="table table-bordered table-hover table-striped">
     <thead>
---
+++ pager.2.1.js
@@ -0,0 +1,144 @@
+/*
+ *Name: BootStrap Pager 
+ *Author: CooMark
+ *version: 2.1
+ *BootStrap version: 3.0
+ */
+(function($) {
+    $.fn.pagingTable = function(options) {
+        var table = $(this);
+        var settings = $.extend({
+            json_url: null,
+            pageSize: 10,
+            separator: "#"
+        }, options);
+
+        if (!table.is("table")) {
+            return this;
+        }
+
+        table.data({
+            pagesize: settings.pageSize,
+            currentpage: 1,
+            dataurl: settings.json_url
+        });
+
+        var load_json = function(currentpage) {
+            var currentpage = currentpage || 1;
+            $.ajax({
+                url: settings.json_url,
+                method: 'get',
+                data: 'pagesize=' + settings.pageSize + '&currentpage=' + currentpage,
+                success: function(resp) {
+                        var re_pagebar = table.data('totalrecords') == resp.totalrecords ? 0 : 1;
+                        table.data('totalrecords', resp.totalrecords);
+                        var data = resp.data;
+                        table.data('totalpages', Math.ceil(resp.totalrecords / settings.pageSize));
+                        table.find('tbody tr[template!=1]').remove();
+
+                        //re-generate the paging bar
+                        var template = table.find("tbody tr[template=1]");
+                        var tds = template.find('td');
+
+                        var datarow = (template.clone())
+                            .removeAttr('template')
+                            .removeAttr('style');
+
+                        var template_str = $("<div></div>").append(datarow).html();
+
+                        for (var dr = 0; dr < data.length; dr++) {
+                            var newrow = template_str;
+                            for (var field in data[dr]) {
+                                var reg = new RegExp(settings.separator + field + settings.separator, 'ig');
+                                newrow = newrow.replace(reg, data[dr][field]);
+                            }
+                            table.find('tbody').append(newrow);
+                        }
+
+                        if (re_pagebar) {
+                            getPageBar(currentpage);
+                        }
+                    } //end success
+            });
+        }
+        load_json(1);
+
+        var getPageBar = function(currentpage) {
+            var currentpage = currentpage || 1;
+            totalpages = table.data('totalpages');
+            var tmp = '';
+            tmp += "<nav>";
+            tmp += "    <ul class='pagination'>";
+            tmp += "        <li class='disabled first'><a href='#' aria-label='First'><span class='glyphicon glyphicon-step-backward'></span></a></li>";
+            tmp += "        <li class='disabled previous'><a href='#' aria-label='Next'><span class='glyphicon glyphicon-chevron-left'></span></a></li>";
+            tmp += "        <li class='active'><a href='#'>1</a></li>";
+
+            var header = tmp;
+            var pagers = "";
+
+            for (i = 2; i <= totalpages; i++) {
+                pagers = pagers + "<li><a href='#'>" + i + "</a></li>";
+            }
+
+            tmp = ''
+            tmp += "        <li class='next'><a href='#' aria-label='Previous'><span class='glyphicon glyphicon-chevron-right'></span></a></li>";
+            tmp += "        <li class='last'><a href='#' aria-label='First'><span class='glyphicon glyphicon-step-forward'></span></a></li>";
+            tmp += "    </ul>";
+            tmp += "</nav>";
+            var footer = tmp;
+
+            var barName = 'pagerbar_' + table.attr("id");
+            $("#" + barName).remove();
+
+            var pagerbar = $("<div></div>").attr("id", barName);
+            pagerbar.html(header + pagers + footer).insertAfter(table);
+
+            pagerbar.find('li').on("click", function() {
+                page_click(this);
+            });
+
+        };
+
+        var page_click = function(obj) {
+            if ($(obj).hasClass('disabled')) {
+                return false;
+            }
+
+            var pagerbar = $(obj).parents('ul');
+            var page_index = table.data('currentpage');
+
+            if ($(obj).hasClass('previous')) {
+                page_index = page_index - 1;
+            } else if ($(obj).hasClass('next')) {
+                page_index = page_index + 1;
+            } else if ($(obj).hasClass('first')) {
+                page_index = 1;
+            } else if ($(obj).hasClass('last')) {
+                page_index = table.data('totalpages');
+            } else {
+                page_index = $(obj).find('a').html();
+            }
+
+            pagerbar.find('li').removeClass('disabled').removeClass('active');
+
+            if (page_index <= 1) {
+                page_index = 1;
+                pagerbar.find('li.previous, li.first').addClass('disabled');
+            }
+            if (page_index >= table.data('totalpages')) {
+                page_index = table.data('totalpages');
+                pagerbar.find('li.next, li.last').addClass('disabled');
+            }
+
+            page_index = parseInt(page_index);
+            table.data('currentpage', page_index);
+            pagerbar.find('li').eq(page_index + 1).addClass('active');
+
+            load_json(page_index);
+        };
+
+
+        return this;
+    };
+
+})($);
